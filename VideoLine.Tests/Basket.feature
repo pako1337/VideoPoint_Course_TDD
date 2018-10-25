5856be5497d4334a087e6d8e8b3da0a21cea0730
pako1337 <pako1337@gmail.com>
2018-10-25 17:47:42 +0200

    04.01 Wprowadzenie do SpecFlow

---
+++ VideoLine.Tests/Basket.feature
@@ -0,0 +1,10 @@
+﻿Feature: Basket
+	In order to support my business
+	I want to have functional basket
+	so customers can buy my products
+
+@mytag
+Scenario: Add course to basket
+	Given empty basket
+	When I add course to basket
+	Then there should be 1 item in basket
---
+++ VideoLine.Tests/BasketSteps.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Should;
+using TechTalk.SpecFlow;
+using VideoLine.Models;
+
+namespace VideoLine.Tests
+{
+    [Binding]
+    public class BasketSteps
+    {
+        private Basket basket;
+
+        [Given(@"empty basket")]
+        public void GivenEmptyBasket()
+        {
+            basket = new Basket(TaxLocation.None, null);
+        }
+        
+        [When(@"I add course to basket")]
+        public void WhenIAddCourseToBasket()
+        {
+            basket.Add(new Course());
+        }
+        
+        [Then(@"there should be (.*) item in basket")]
+        public void ThenThereShouldBeItemInBasket(int p0)
+        {
+            basket.Count().ShouldEqual(1);
+        }
+    }
+}
